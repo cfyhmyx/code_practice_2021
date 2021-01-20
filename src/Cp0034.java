b984bf92392350fc79f8fcf7a68536d8911efb17
cfyhmyx <cfyhmyx@gmail.com>
2021-01-20 14:42:49 -0800

    binary search

--- src/Cp0034.java
+++ src/Cp0034.java
@@ -3,17 +3,51 @@
 public class Cp0034 {
     public static void main(String args[]) {
         Cp0034 cp = new Cp0034();
-        int[] nums = {5, 7, 7, 8, 8, 10};
-        int[] result = cp.searchRange(nums, 6);
+        int[] nums = {8};
+        int[] result = cp.searchRange(nums, 8);
         for (int i : result) {
             System.out.println(i);
         }
     }
 
     // Main idea: binary search.
-    // Time: O(n).
+    // Time: O(log(n)).
     // Space: O(1).
     public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[left] != target) {
+            return new int[]{-1, -1};
+        }
+        int[] result = {left, right};
+        right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2 + 1;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        result[1] = right;
+        return result;
+    }
+
+    // Main idea: binary search.
+    // Time: O(n).
+    // Space: O(1).
+    /*public int[] searchRange(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
@@ -39,5 +73,5 @@ public class Cp0034 {
         } else {
             return new int[]{-1, -1};
         }
-    }
+    }*/
 }
