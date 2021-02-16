c3db972af775c8a7f835f895e9bf30826d0f7ba0
cfyhmyx <cfyhmyx@gmail.com>
2021-02-16 14:52:46 -0800

    dp

---
+++ src/Cp0516.java
@@ -0,0 +1,26 @@
+public class Cp0516 {
+    public static void main(String args[]) {
+        Cp0516 cp = new Cp0516();
+        int result = cp.longestPalindromeSubseq("bbbab");
+        System.out.println(result);
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+        for (int i=1; i<=n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int len= 2; len<=n;len++) {
+            for(int i=1; i+len-1 <= n; i++) {
+                int j = i+len-1;
+                if (s.charAt(i-1) == s.charAt(j-1)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[1][n];
+    }
+}
