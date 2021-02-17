e6585e40b6c0f40a16e2ed2f36ab2848c30116a5
cfyhmyx <cfyhmyx@gmail.com>
2021-02-16 17:46:25 -0800

    dp, divide and conquer

--- src/Cp0312.java
+++ src/Cp0312.java
@@ -8,10 +8,32 @@ public class Cp0312 {
         System.out.println(result);
     }
 
-    // Main idea: divide and conquer.
+    // Main idea: dp.
     // Time: O(n^3).
     // Space: O(n^2).
     public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
+        }
+        arr[0] = arr[n + 1] = 1;
+        int[][] dp = new int[n + 2][n + 2];
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i + len - 1 <= n; i++) {
+                int j = i + len - 1;
+                for (int k = i; k <= j; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k - 1] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j + 1]);
+                }
+            }
+        }
+        return dp[1][n];
+    }
+
+    // Main idea: divide and conquer.
+    // Time: O(n^3).
+    // Space: O(n^2).
+    /*public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n + 2];
         for (int i = 0; i < n; i++) {
@@ -31,5 +53,5 @@ public class Cp0312 {
                     + helper(x + 1, j, nums, dp));
         }
         return dp[i][j];
-    }
+    }*/
 }
