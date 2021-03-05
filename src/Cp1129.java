e67d09f9c88c642149d30de2b5a9564bd28184b4
cfyhmyx <cfyhmyx@gmail.com>
2021-03-04 23:01:04 -0800

    bfs

---
+++ src/Cp1129.java
@@ -0,0 +1,75 @@
+// https://leetcode.com/problems/shortest-path-with-alternating-colors/
+
+import java.util.*;
+
+public class Cp1129 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: bfs.
+    // Time: O(V + E).
+    // Space: O(E).
+    public int[] shortestAlternatingPaths(int n, int[][] red_edges,
+                                          int[][] blue_edges) {
+        int[][] graph = new int[n][n];
+        buildGraph(graph, n, red_edges, blue_edges);
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 1});
+        q.offer(new int[]{0, -1});
+        int len = 0;
+        int[] res = new int[n];
+        Arrays.fill(res, Integer.MAX_VALUE);
+        res[0] = 0;
+
+        Set<String> visited = new HashSet<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            len++;
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+                int node = cur[0];
+                int color = cur[1];
+                int oppoColor = -color;
+
+                for (int j = 1; j < n; j++) {
+                    if (graph[node][j] == oppoColor ||
+                            graph[node][j] == 0) {
+                        if (!visited.add(j + "" + oppoColor)) continue;
+                        q.offer(new int[]{j, oppoColor});
+                        res[j] = Math.min(res[j], len);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (res[i] == Integer.MAX_VALUE) {
+                res[i] = -1;
+            }
+        }
+
+        return res;
+    }
+
+    private void buildGraph(int[][] graph, int n,
+                            int[][] red_edges, int[][] blue_edges) {
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+        for (int[] e : red_edges) {
+            int from = e[0];
+            int to = e[1];
+            graph[from][to] = 1;
+        }
+        for (int[] e : blue_edges) {
+            int from = e[0];
+            int to = e[1];
+            if (graph[from][to] == 1) {
+                graph[from][to] = 0;
+            } else {
+                graph[from][to] = -1;
+            }
+        }
+    }
+}
