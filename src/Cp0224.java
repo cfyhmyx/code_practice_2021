21785864ca5510484b6b0988ed8bd17c96c52e8b
cfyhmyx <cfyhmyx@gmail.com>
2021-05-16 22:01:29 -0700

    stack

---
+++ src/Cp0224.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/basic-calculator/
+
+import java.util.Stack;
+
+public class Cp0224 {
+    public static void main(String args[]) {
+        Cp0224 cp = new Cp0224();
+        int result = cp.calculate("(1+(4+5+2)-3)+(6+8)");
+        System.out.print(result);
+    }
+
+    // Main idea: stack.
+    // Time: O(n).
+    // Space: O(n).
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int num = s.charAt(i) - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                res += num * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                res = res * stack.pop() + stack.pop();
+            }
+        }
+        return res;
+    }
+}
