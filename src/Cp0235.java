51635e7b68671b53f97c0ccbbc4114463060fd4f
cfyhmyx <cfyhmyx@gmail.com>
2021-06-30 20:00:52 -0700

    tree, post order

---
+++ src/Cp0235.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+public class Cp0235 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: tree, post order.
+    // Time: O(n).
+    // Space: O(n).
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q) return root;
+        if (root == null) return null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
