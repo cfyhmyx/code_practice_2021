81391c8c1893583dbc8a9fa819f82d5dd9f9c387
cfyhmyx <cfyhmyx@gmail.com>
2021-07-01 21:22:41 -0700

    bit, math

---
+++ src/Cp0089.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/gray-code/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cp0089 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: bit, math.
+    // Time: O(1<<n).
+    // Space: O(1).
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 1 << n; i++) {
+            result.add(i ^ i >> 1);
+        }
+        return result;
+    }
+}
