309b2bf2e1baff7f5eba03f5b018d9b81582a0cb
cfyhmyx <cfyhmyx@gmail.com>
2021-09-12 21:08:23 -0700

    array

---
+++ src/Cp0036.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/valid-sudoku/
+
+import java.util.HashSet;
+
+public class Cp0036 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: array.
+    // Time: O(1).
+    // Space: O(1).
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            HashSet<Character> rows = new HashSet<>();
+            HashSet<Character> columns = new HashSet<>();
+            HashSet<Character> cube = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && !rows.add(board[i][j]))
+                    return false;
+                if (board[j][i] != '.' && !columns.add(board[j][i]))
+                    return false;
+                int RowIndex = 3 * (i / 3);
+                int ColIndex = 3 * (i % 3);
+                if (board[RowIndex + j / 3][ColIndex + j % 3] != '.' && !cube.add(board[RowIndex + j / 3][ColIndex + j % 3]))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
