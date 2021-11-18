404fa9c1c74d73657c4d1633e9aff8e56e442bbc
cfyhmyx <cfyhmyx@gmail.com>
2021-11-17 20:38:20 -0800

    math

---
+++ src/Cp1007.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+
+public class Cp1007 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: math.
+    // Time: O(n).
+    // Space: O(1).
+    public int minDominoRotations(int[] A, int[] B) {
+        int[] countA = new int[7]; // countA[i] records the occurrence of i in A.
+        int[] countB = new int[7]; // countB[i] records the occurrence of i in B.
+        int[] same = new int[7]; // same[k] records the occurrence of k, where k == A[i] == B[i].
+        for (int i = 0; i < A.length; ++i) {
+            ++countA[A[i]];
+            ++countB[B[i]];
+            if (A[i] == B[i]) { ++same[A[i]]; }
+        }
+        for (int i = 1; i < 7; ++i) {
+            if (countA[i] + countB[i] - same[i] >= A.length) {
+                return Math.min(countA[i], countB[i]) - same[i];
+            }
+        }
+        return -1;
+    }
+}
