18e0f3a6552d270bf80cf6b531f4b117571a8ac7
cfyhmyx <cfyhmyx@gmail.com>
2021-12-08 20:09:26 -0800

    quick sort

---
+++ src/Cp0912.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/sort-an-array/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cp0912 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: quick sort
+    // Time: O(n) for average.
+    // Space: O(1).
+    public List<Integer> sortArray(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        quickSort(nums, 0, nums.length - 1);
+        for (int i : nums) res.add(i);
+        return res;
+    }
+    private void quickSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+        int mid = partition(nums, l, r);
+        quickSort(nums, l, mid);
+        quickSort(nums, mid + 1, r);
+    }
+    private int partition(int[] nums, int l, int r) {
+        int pivot = nums[l];
+        while (l < r) {
+            while (l < r && nums[r] >= pivot) r--;
+            nums[l] = nums[r];
+            while (l < r && nums[l] <= pivot) l++;
+            nums[r] = nums[l];
+        }
+        nums[l] = pivot;
+        return l;
+    }
+}
