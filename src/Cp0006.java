055a7104bbef6957bc389b18762c0b1a9526b1da
cfyhmyx <cfyhmyx@gmail.com>
2022-01-08 23:55:49 -0800

    string

---
+++ src/Cp0006.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/zigzag-conversion/
+
+public class Cp0006 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: string
+    // Time: O(n).
+    // Space: O(n).
+    public String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuffer();
+        }
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) {
+                // vertically down
+                sb[idx].append(c[i++]);
+            }
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--) {
+                // obliquely up
+                sb[idx].append(c[i++]);
+            }
+        }
+        for (int idx = 1; idx < sb.length; idx++) {
+            sb[0].append(sb[idx]);
+        }
+        return sb[0].toString();
+    }
+}
