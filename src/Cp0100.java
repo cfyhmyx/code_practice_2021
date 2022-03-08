84c74eee606461a57c5f9454163f32e00830695f
cfyhmyx <cfyhmyx@gmail.com>
2022-03-07 23:33:41 -0800

    tree, recursion

---
+++ src/Cp0100.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/same-tree/
+
+public class Cp0100 {
+    public static void main(String args[]) {
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        else if (p == null || q==null || p.val != q.val) return false;
+        else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
