215c9e6e18520e02dbb10cdf63d1460913b7f25c
cfyhmyx <cfyhmyx@gmail.com>
2022-03-27 21:34:54 -0700

    string

---
+++ src/Cp0165.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/compare-version-numbers/
+
+public class Cp0165 {
+    public static void main(String args[]) {
+    }
+
+    // Main idea: string
+    // Time: O(n).
+    // Space: O(1).
+    public int compareVersion(String version1, String version2) {
+        int len1 = version1.length(), len2 = version2.length();
+        int i = 0, j = 0;
+        while (i < len1 || j < len2) {
+            int temp1 = 0;
+            int temp2 = 0;
+            while (i < len1 && version1.charAt(i) != '.') {
+                temp1 = temp1 * 10 + version1.charAt(i++) - '0';
+
+            }
+            while (j < len2 && version2.charAt(j) != '.') {
+                temp2 = temp2 * 10 + version2.charAt(j++) - '0';
+
+            }
+            if (temp1 > temp2) return 1;
+            else if (temp1 < temp2) return -1;
+            else {
+                i++;
+                j++;
+
+            }
+        }
+        return 0;
+    }
+}
